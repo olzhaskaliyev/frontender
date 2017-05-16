055eb82b194511df31e9d746065557c842784e70
Olzhas Kaliyev <Olzhas.Kaliyev.90@Gmail.com>
2017-05-16 16:29:24 +0600

    1) Clean up 2) rem to px 3) normalize to libraries

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,3 @@
 $(document).ready(function () {
-    var footerHeight = $('footer').innerHeight();
-    $('body').css({
-        'paddingBottom': footerHeight
-    });
 
-    $(document).on('click', '.ui-widget-overlay', function () {
-        $('.ui-dialog-content').dialog('close');
-    });
-
-    $('.slider').slick({
-        nextArrow: '<div class="slick-next"></div>',
-        prevArrow: '<div class="slick-prev"></div>'
-    });
-
-    $('.carousel').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        infinite: true,
-        nextArrow: '<div class="slick-next"></div>',
-        prevArrow: '<div class="slick-prev"></div>',
-    });
-
-    $(document).on('click', '.burger', function () {
-        $('.mobile-menu').toggleClass('active');
-    });
 });
