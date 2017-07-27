30eed582189d087fe21393ddfc210b4cbae0f0f5
Olzhas Kaliyev <Olzhas.Kaliyev.90@Gmail.com>
2017-07-27 19:09:20 +0600

    refactored! B)

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-// Подключение плагинов
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
 const runSequence = require('run-sequence');
@@ -8,56 +7,8 @@ const mainBowerFiles = require('main-bower-files');
 const browserSync = require('browser-sync').create();
 let dev = true;
 
-// Интеграция библиотек
-gulp.task('vendors', () => {
-    gulp.src('src/views/*.html')
-        .pipe(wiredep({ignorePath: /^(\.\.\/)*\.\./}))
-        .pipe(gulp.dest('src/views'));
-
-    gulp.src(mainBowerFiles(['**/*.{jpg,png,svg,gif,webp}', '!fonts/*'], function (err) {
-    }))
-        .pipe(gulp.dest('src/images'));
-
-    gulp.src(mainBowerFiles('**/*.{eot,svg,otf,ttf,woff,woff2}', function (err) {
-    }))
-        .pipe(gulp.dest('src/fonts'));
-});
-
-// Оптимизация картинок
-gulp.task('images', () => {
-    return gulp.src('src/images/**/*')
-        .pipe($.cache($.imagemin()))
-        .pipe(gulp.dest('dist/images'));
-});
-
-// Сборка шрифтов
-gulp.task('fonts', () => {
-    return gulp.src('src/fonts/**/*')
-        .pipe(gulp.dest('dist/fonts'));
-});
-
-// Минификация и Конкатинация скриптов и стилей, сборка HTML
-gulp.task('views', ['templates', 'styles', 'scripts'], () => {
-    return gulp.src('.tmp/**/*.html')
-        .pipe($.useref({searchPath: ['.tmp', 'src', '.']}))
-        //.pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
-        .pipe($.if(/\.html$/, $.fileInclude({prefix: '@@', basepath: 'src/views/'})))
-        .pipe($.if(/\.css$/, $.cssnano({safe: true, autoprefixer: false})))
-        .pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}})))
-        .pipe(gulp.dest('dist'));
-});
-
-// Интеграция шаблонов
-gulp.task('templates', () => {
-    return gulp.src('src/*.html')
-        .pipe($.fileInclude({prefix: '@@', basepath: 'src/views/'}))
-        .pipe(gulp.dest('.tmp'))
-        .pipe(browserSync.reload({stream: true}));
-});
-
-// Задача по умолчанию (gulp)
 gulp.task('default', () => {
-    runSequence('clean', 'vendors', ['templates', 'styles', 'scripts'], () => {
+    runSequence('clean', 'bower', ['views', 'styles', 'scripts'], () => {
         browserSync.init({
             notify: false,
             port: 9000,
@@ -69,11 +20,10 @@ gulp.task('default', () => {
             }
         });
 
-        gulp.watch('bower.json', ['vendors']);
-        gulp.watch('src/**/*.html', ['templates']);
+        gulp.watch('bower.json', ['wiredep', 'bower']);
+        gulp.watch('src/**/*.html', ['views']);
         gulp.watch('src/styles/**/*.scss', ['styles']);
         gulp.watch('src/scripts/**/*.js', ['scripts']);
-        gulp.watch('src/fonts/**/*', ['fonts']);
         gulp.watch(['src/images/**/*', 'src/fonts/**/*']).on('change', browserSync.reload);
     });
 });
@@ -81,11 +31,10 @@ gulp.task('default', () => {
 gulp.task('build', () => {
     return new Promise(resolve => {
         dev = false;
-        runSequence('clean', 'vendors', ['views', 'images', 'fonts', 'extras'], resolve);
+        runSequence('clean', ['minimization', 'images', 'fonts', 'extras'], resolve);
     });
 });
 
-// Обзор собранной верстки
 gulp.task('serve:dist', ['build'], () => {
     browserSync.init({
         notify: false,
@@ -96,12 +45,40 @@ gulp.task('serve:dist', ['build'], () => {
     });
 });
 
-// Очистка места сборки
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
-// Компиляция SASS и автопрефикс
+gulp.task('wiredep', () => {
+    gulp.src('src/**/*.html')
+        .pipe(wiredep({ignorePath: /^(\.\.\/)*\.\./}))
+        .pipe(gulp.dest('src/'));
+});
+
+gulp.task('bower', () => {
+    gulp.src(mainBowerFiles(['**/*.{jpg,png,svg,gif,webp}', '!fonts/*'], function (err) {}))
+        .pipe(gulp.dest('src/images'));
+
+    gulp.src(mainBowerFiles('**/*.{eot,svg,otf,ttf,woff,woff2}', function (err) {}))
+        .pipe(gulp.dest('src/fonts'));
+});
+
+gulp.task('minimization', ['views', 'styles', 'scripts'], () => {
+    return gulp.src('.tmp/*.html')
+        .pipe($.useref({searchPath: ['.tmp', 'src', '.']}))
+        .pipe($.if(/\.css$/, $.cssnano({safe: true, autoprefixer: false})))
+        .pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}})))
+        .pipe(gulp.dest('dist'))
+        .pipe($.size({title: 'minimization', showFiles: true}));
+});
+
+gulp.task('views', ['wiredep'], () => {
+    gulp.src('src/*.html')
+        .pipe($.fileInclude({prefix: '@@', basepath: 'src/views/'}))
+        .pipe(gulp.dest('.tmp'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
 gulp.task('styles', () => {
-    return gulp.src('src/styles/main.scss')
+    return gulp.src('src/styles/*.scss')
         .pipe($.plumber())
         .pipe($.if(dev, $.sourcemaps.init()))
         .pipe($.sass.sync({
@@ -115,7 +92,6 @@ gulp.task('styles', () => {
         .pipe(browserSync.reload({stream: true}));
 });
 
-// Компиляция Babel (ECMAScript 2015)
 gulp.task('scripts', () => {
     return gulp.src('src/scripts/**/*.js')
         .pipe($.plumber())
@@ -126,7 +102,21 @@ gulp.task('scripts', () => {
         .pipe(browserSync.reload({stream: true}));
 });
 
-// Сборка разного в корне (favicon, robots, etc.)
+gulp.task('images', () => {
+    return gulp.src(mainBowerFiles(['**/*.{jpg,png,svg,gif,webp}', '!fonts/*'], function (err) {})
+        .concat('src/images/**/*'))
+        .pipe($.cache($.imagemin()))
+        .pipe(gulp.dest('dist/images'))
+        .pipe($.size({title: 'images', showFiles: true}));
+});
+
+gulp.task('fonts', () => {
+    return gulp.src(mainBowerFiles('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
+        .concat('src/fonts/**/*'))
+        .pipe(gulp.dest('dist/fonts'))
+        .pipe($.size({title: 'fonts'}));
+});
+
 gulp.task('extras', () => {
     return gulp.src([
         'src/*',
